d63de91ceca0662513eca1d3557e10b298cd723f
francisco328react <fseveriano56@hotmail.com>
2025-07-09 13:41:41 -0400

    feat: adicionando na courierRoutes.ts put e delete

--- backend/src/routes/courierRoutes.ts
+++ backend/src/routes/courierRoutes.ts
@@ -1,10 +1,12 @@
 import { Router, Response, Request } from "express";
-import { createCourier, getAllCouriers } from "../controllers/courierController";
+import { createCourier, deleteCourier, getAllCouriers, updateCourier } from "../controllers/courierController";
 import { esureAuth } from "../middlewares/ensureAuth";
 
 const router = Router();
 
 router.get("/couriers", esureAuth, getAllCouriers)
 router.post("/couriers", esureAuth, createCourier)
+router.put("/couriers/:id", esureAuth, updateCourier)
+router.delete("/couriers/:id", esureAuth, deleteCourier)
 
 export default router;
