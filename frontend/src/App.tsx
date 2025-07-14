1205cd7218ef522b3c10b37b55362df31482c134
francisco328react <fseveriano56@hotmail.com>
2025-07-13 21:41:49 -0400

    chore: centralizando configuração de rotas em routes.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,9 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Login } from "./pages/Login";
-import { useAuth } from './context/useAuth';
-
-type Props = {
-  children: React.ReactNode;
-};
+import { useRoutes } from "react-router-dom";
+import { routes } from "./routes/routes";
 
 export function App() {
+  const routing = useRoutes(routes);
+  return routing;
 
-  function Dashboard() {
-    return <h1 className='text-center mt-20 text-2xl font-bold'>Dashboard</h1>
-  }
-
-  function PrivateRoute({ children }: Props) {
-    const { isAuthenticated } = useAuth();
-    return isAuthenticated ? children : <Navigate to="/login" />
-  }
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />}/>
-        <Route 
-          path='/dashboard'
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes> 
-    </BrowserRouter>
-  )
 }
 
---
+++ frontend/src/routes/routes.tsx
@@ -0,0 +1,18 @@
+import type { RouteObject } from "react-router-dom";
+import { Login } from "../pages/Login";
+//import { Dashboard } from "../pages/Dashboard";
+
+export const routes: RouteObject[] = [
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard", 
+    //element: (<Dashboard />),
+  },
+  {
+    path: "*",
+    element: <Login />,
+  },
+];
