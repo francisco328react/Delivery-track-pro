f5d29f6a7b0d1f40420cd4c60e712722f33da7e5
francisco328react <fseveriano56@hotmail.com>
2025-07-15 10:42:38 -0400

    fix: adicionando /:id nas rotas de edição e exclusão de entregas

--- backend/src/routes/deliveryRoutes.ts
+++ backend/src/routes/deliveryRoutes.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get("/deliveries", ensureAuth, getAllDeliveries);
 router.post("/deliveries", ensureAuth, createDelivery);
-router.put("/deliveries", ensureAuth, updateDelivey);
-router.delete("/deliveries", ensureAuth, deleteDelivery);
+router.put("/deliveries/:id", ensureAuth, updateDelivey);
+router.delete("/deliveries/:id", ensureAuth, deleteDelivery);
 
 export default router;
